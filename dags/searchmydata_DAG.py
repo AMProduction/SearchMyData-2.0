5b022de75b1ada432b45cdbafbde301a8422f24c
Andrii Malchyk <snooki17@gmail.com>
2023-07-21 16:38:47 +0300

    Migrated to Airflow
    Signed-off-by: andrii_malchyk <snooki17@gmail.com>

--- dags/searchmydata_DAG.py
+++ dags/searchmydata_DAG.py
@@ -4,6 +4,9 @@ import pendulum
 
 from airflow.decorators import dag, task
 from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+
+connection_string = Variable.get("mongo_connection_string")
 
 
 @dag(dag_id='SearchMyData_ETL',
@@ -13,12 +16,35 @@ from airflow.models import Variable
      tags=["searchmydata"],
      max_active_runs=1,
      )
-@task()
-def extract_missing_person_register():
-    connection_string = Variable.get("mongo_connection_string")
-    from modules.MissingPersonsRegister import MissingPersonsRegister
-    missing_persons = MissingPersonsRegister(connection_string)
-    missing_persons.setup_dataset()
+def searchmydata_etl():
+    start = EmptyOperator(task_id="start")
+    stop = EmptyOperator(task_id="stop")
+
+    @task()
+    def extract_missing_persons_register():
+        from modules.MissingPersonsRegister import MissingPersonsRegister
+        missing_persons = MissingPersonsRegister(connection_string)
+        missing_persons.setup_dataset()
+
+    @task()
+    def extract_wanted_persons_register():
+        from modules.WantedPersonsRegister import WantedPersonsRegister
+        wanted_persons = WantedPersonsRegister(connection_string)
+        wanted_persons.setup_dataset()
+
+    @task()
+    def extract_debtors_register():
+        from modules.DebtorsRegister import DebtorsRegister
+        debtors = DebtorsRegister(connection_string)
+        debtors.setup_dataset()
+
+    @task()
+    def extract_lustrated_persons_register():
+        from modules.LustratedPersonsRegister import LustratedPersonsRegister
+        lustrated = LustratedPersonsRegister(connection_string)
+        lustrated.setup_dataset()
+
+    start >> [extract_missing_persons_register(), extract_wanted_persons_register(), extract_lustrated_persons_register(), extract_debtors_register()] >> stop
 
 
-extract_missing_person_register()
+searchmydata_etl()
