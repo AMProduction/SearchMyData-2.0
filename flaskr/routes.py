167bdabb50239c751b2a727dda4858f12bde57a5
Andrii Malchyk <snooki17@gmail.com>
2023-07-28 17:07:32 +0300

    Refactored search results representation
    Signed-off-by: andrii_malchyk <snooki17@gmail.com>

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -15,46 +15,26 @@ def home():
 
 
 @app.route('/info')
-def get_collections_info():
+def show_collections_info():
+    from .src.data_action import get_registers_info, check_is_expired
     db = mongo[os.getenv('MONGO_INITDB_DATABASE')]
     service_collection = db['ServiceCollection']
-    registers_info = service_collection.find({},
-                                             {'_id': 1, 'Description': 1, 'DocumentsCount': 1, 'LastModifiedDate': 1}) \
-        .sort([('_id', 1)])
-    expiration = False
-    expired_time = datetime.now() - timedelta(days=2)
-    for record in service_collection.find():
-        last_modified_date = datetime.strptime(record['LastModifiedDate'], '%Y-%m-%d %H:%M:%S.%f')
-        if last_modified_date < expired_time:
-            expiration = True
+    registers_info = get_registers_info(service_collection)
+    expiration = check_is_expired(service_collection)
     return render_template('info.html', result=registers_info, isExpired=expiration, now=datetime.utcnow())
 
 
 @app.route('/result', methods=['POST', 'GET'])
-def get_search_results():
-    from .src.DebtorsRegister import DebtorsRegister
-    from .src.EntrepreneursRegister import EntrepreneursRegister
-    from .src.LegalEntitiesRegister import LegalEntitiesRegister
-    from .src.MissingPersonsRegister import MissingPersonsRegister
-    from .src.WantedPersonsRegister import WantedPersonsRegister
-    from .src.LustratedPersonsRegister import LustratedPersonsRegister
-    # create instances
-    missing_persons = MissingPersonsRegister()
-    wanted_persons = WantedPersonsRegister()
-    debtors = DebtorsRegister()
-    legal_entities = LegalEntitiesRegister()
-    entrepreneurs = EntrepreneursRegister()
-    lustrated = LustratedPersonsRegister()
+def show_search_results():
+    from .src.data_action import search_into_collection
     if request.method == 'POST':
         search_string = request.form['search']
+        db = mongo[os.getenv('MONGO_INITDB_DATABASE')]
         # call search methods
-        result_missing_persons = missing_persons.search_into_collection(search_string)
-        result_wanted_persons = wanted_persons.search_into_collection(search_string)
-        result_debtors = debtors.search_into_collection(search_string)
-        result_legal_entities = legal_entities.search_into_collection(search_string)
-        result_entrepreneurs = entrepreneurs.search_into_collection(search_string)
-        result_lustrated = lustrated.search_into_collection(search_string)
-        return render_template('result.html', now=datetime.utcnow(), resultMissingPersons=result_missing_persons,
-                               resultWantedPersons=result_wanted_persons, resultDebtors=result_debtors,
-                               resultLegalEntities=result_legal_entities, resultEntrepreneurs=result_entrepreneurs,
-                               resultLustrated=result_lustrated)
+        result_missing_persons = search_into_collection(db['MissingPersons'], search_string)
+        result_wanted_persons = search_into_collection(db['WantedPersons'], search_string)
+        result_debtors = search_into_collection(db['Debtors'], search_string)
+        result_lustrated = search_into_collection(db['Lustrated'], search_string)
+        return render_template('result.html', now=datetime.utcnow(), result_MissingPersons=result_missing_persons,
+                               result_WantedPersons=result_wanted_persons, result_Debtors=result_debtors,
+                               result_Lustrated=result_lustrated)
