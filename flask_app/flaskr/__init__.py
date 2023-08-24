2fc830c741c355b3d6733adcbefadd906b2eabcc
Andrii Malchyk <snooki17@gmail.com>
2023-08-24 20:53:15 +0300

    Added mongoengine to the requirements.txt. Added the RegisterInfo model
    Signed-off-by: andrii_malchyk <snooki17@gmail.com>

--- flask_app/flaskr/__init__.py
+++ flask_app/flaskr/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from pymongo import MongoClient
+from mongoengine import connect
 
 from config import Config
 
@@ -14,5 +15,6 @@ csrf = CSRFProtect(app)
 csrf.init_app(app)
 
 mongo = MongoClient(os.getenv('MONGO_URI'))
+connect(host=os.getenv('MONGO_URI'))
 
 from flaskr import routes
